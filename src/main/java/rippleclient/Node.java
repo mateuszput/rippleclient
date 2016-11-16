49e9a12ea424ef97e3e7cb8d9b95a066f1679a41
Mateusz Put <mateusz.put@gmail.com>
2016-11-16 23:00:22 +0100

    client

--- src/main/java/rippleclient/Node.java
+++ src/main/java/rippleclient/Node.java
@@ -1,5 +1,10 @@
 package rippleclient;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,20 +12,74 @@ import java.util.List;
  * Hello world!
  *
  */
-public class Node
+public class Node extends Thread
 {
     private Integer port;
     private List<Integer> unl;
+    private ServerSocket serverSocket;
 
-    public static void main( String[] args )
-    {
-        Node node = new Node();
-        node.setPorts(args);
+
+    // listening
+    public void run() {
+        System.out.println("Waiting for client on port " +
+                serverSocket.getLocalPort() + "...");
+        Socket server = null;
+
+        while(true) {
+            try {
+                server = serverSocket.accept();
+
+                System.out.println("Just connected to " + server.getRemoteSocketAddress());
+                DataInputStream in = new DataInputStream(server.getInputStream());
+                System.out.println(in.readUTF());
+
+
+                // parse string
+                // store
+                // if wszystkie UNL, to decyzja
+
+//                DataOutputStream out = new DataOutputStream(server.getOutputStream());
+//                out.writeUTF("Thank you for connecting to " + server.getLocalSocketAddress()
+//                        + "\nGoodbye!");
+
+
+
+            }catch(SocketTimeoutException s) {
+                System.out.println("Socket timed out!");
+                break;
+            }catch(IOException e) {
+                e.printStackTrace();
+                break;
+            } finally {
+                try {
+                    server.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 
-    public void setPorts(String[] args){
+    public static void main( String[] args )
+    {
+
+        try {
+//            Thread t = new Node();
+            Node node = new Node();
+            node.setPorts(args);
+            node.start();
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void setPorts(String[] args) throws IOException{
+
         port = new Integer(args[0]);
+        serverSocket = new ServerSocket(port);
+
         unl = new ArrayList<Integer>();
 
 
